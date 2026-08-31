c0d8c32f14d87deabd5219295913c91cfb8a2d30
Eteral <eteralwork@gmail.com>
2026-08-31 22:41:54 +0530

    blog

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from './LandingPage.tsx';
-import PrivacyPolicy from './pages/PrivacyPolicy.tsx';
-import TermsAndConditions from './pages/TermsAndConditions.tsx';
 import { AuthProvider } from './lib/auth.tsx';
 import './index.css';
 
@@ -11,14 +9,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-        </Routes>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
 );
-// src/main.jsx or src/App.jsx
-
